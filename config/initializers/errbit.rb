5b5060c7d0585a3ea2c469981065b08c097ef3e7
Andrea Zaupa <andrea.zaupa@develon.com>
2011-07-26 17:37:03 +0200

    errbit key

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,6 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = 'e02080324fc912965015d7a66deefdfb'
+  config.host    = 'errors.lab.develon.com'
+  config.port    = 80
+  config.secure  = config.port == 443
+end
