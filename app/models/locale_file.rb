b2ddb62ddfd867aa8be9ef1149c0c4e8901ef7d2
Andrea Zaupa <andrea.zaupa@develon.com>
2011-07-27 09:38:23 +0200

    deep stringify

--- app/models/locale_file.rb
+++ app/models/locale_file.rb
@@ -6,12 +6,12 @@ class LocaleFile < ActiveRecord::Base
   validates_presence_of :asset
 
   before_validation :update_asset_attributes
-  
-  
+
+
   def get_hash
-   self.asset ? YAML.load_file(self.asset.path) : {}
+    self.asset ? YAML.load_file(self.asset.path) : {}
   end
-  
+
   def save_shallow_hash_with_locale(shallow_hash)
     loc_hash={}
     shallow_hash.each do |k,v|
@@ -19,55 +19,66 @@ class LocaleFile < ActiveRecord::Base
     end
     self.save_shallow_hash loc_hash
   end
-  
+
   def save_shallow_hash(shallow_hash)
-    hash=self.class.to_deep_hash(shallow_hash)    
+    hash=self.class.to_deep_hash(shallow_hash) 
+    hash=self.class.deep_stringify_keys(hash)   
     temp_file_name=File.join(Rails.root,"tmp","temp#{Time.now.to_s}#{rand}")
     f=File.new(self.asset.path,"w")
     f.write(hash.to_yaml)
     f.close
   end
-  
-  def save_hash(hash)
-  
-  end
-  
-  def get_shallow_hash
-   h=self.get_hash
-   LocaleFile::to_shallow_hash(h[h.keys.first])
-  end
-  
-  def self.to_shallow_hash(hash)
-     hash.inject({}) do |shallow_hash, (key, value)|
-       if value.is_a?(Hash)
-         to_shallow_hash(value).each do |sub_key, sub_value|
-           shallow_hash[[key, sub_key].join(".")] = sub_value
-         end
-       else
-         shallow_hash[key.to_s] = value
-       end
-       shallow_hash
-     end
-   end
-   # deep_merge by Stefan Rusterholz, see http://www.ruby-forum.com/topic/142809
-   def self.deep_merge!(hash1, hash2)
-     merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
-     hash1.merge!(hash2, &merger)
-   end
-   
-   
-   def self.to_deep_hash(hash)    
-     hash.inject({}) do |deep_hash, (key, value)|
-       keys = key.to_s.split('.').reverse
-       leaf_key = keys.shift
-       key_hash = keys.inject({leaf_key.to_sym => value}) { |hash, key| {key.to_sym => hash} }
-       deep_merge!(deep_hash, key_hash)
-       deep_hash
-     end
-   end
-   
 
-  
+  def save_hash(hash)
+
+  end
+
+  def get_shallow_hash
+    h=self.get_hash
+    LocaleFile::to_shallow_hash(h[h.keys.first])
+  end
+
+  def self.to_shallow_hash(hash)
+    hash.inject({}) do |shallow_hash, (key, value)|
+      if value.is_a?(Hash)
+        to_shallow_hash(value).each do |sub_key, sub_value|
+          shallow_hash[[key, sub_key].join(".")] = sub_value
+        end
+      else
+        shallow_hash[key.to_s] = value
+      end
+      shallow_hash
+    end
+  end
+
+  # deep_merge  vedi http://www.ruby-forum.com/topic/142809
+  def self.deep_merge!(hash1, hash2)
+    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+    hash1.merge!(hash2, &merger)
+  end
+
+
+  def self.deep_stringify_keys(hash)
+    hash.inject({}) { |result, (key, value)|
+      value = deep_stringify_keys(value) if value.is_a? Hash
+      result[(key.to_s rescue key) || key] = value
+      result
+    }
+  end
+
+
+  def self.to_deep_hash(hash)    
+    hash.inject({}) do |deep_hash, (key, value)|
+      keys = key.to_s.split('.').reverse
+      leaf_key = keys.shift
+      key_hash = keys.inject({leaf_key.to_sym => value}) { |hash, key| {key.to_sym => hash} }
+      deep_merge!(deep_hash, key_hash)
+      deep_hash
+    end
+  end
+
+
+
   private
 
   def update_asset_attributes
