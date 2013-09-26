ad1ca2dc1aca4f151903c815ce0af966f0fe408c
andrea <andrea.zaupa@develon.com>
2013-09-26 15:54:17 +0200

    Update schema.rb
    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110727104927) do
+ActiveRecord::Schema.define(:version => 2010727104927) do
 
   create_table "locale_files", :force => true do |t|
     t.string   "original_file"
